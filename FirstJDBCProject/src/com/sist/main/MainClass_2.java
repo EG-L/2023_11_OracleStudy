53a942d9c4d5d1e9af62ba7701241f9766f587fb
pshow <pshow@DESKTOP-9U9F4AS>
2023-11-03 12:10:07 +0900

    20231103

---
+++ FirstJDBCProject/src/com/sist/main/MainClass_2.java
@@ -0,0 +1,51 @@
+package com.sist.main;
+/*
+ *    1)
+ *       Web 개발(사이트 제작)
+ *       
+ *       출력 위치 => 브라우저
+ *                 ------- 오라클 연동하는 프로그램이 없다.
+ *                     HTML / CSS / JavaScript
+ *                     => 외의 모든 언어는 브라우저 일반 텍스트
+ *       브라우저 ===== 자바 ===== 오라클
+ *               전송       읽기
+ *                        JDBC => Java Database Connective
+ *                        java.sql
+ *                        => 오라클 연결하는 프로그램은
+ *                           자바에 존재하지 않는다.
+ *                           => 외부 라이브러리 => ojdbc8.jar
+ *                        파일: xml, json, csv
+ *    => 실무 (X) => 입문
+ *      --------
+ *    => 1차 프로젝트 : 화면 UI / 데이터베이스 (SQL
+ *          HTML/CSS/JavaScript
+ *          => JSP => MVC / Ajax
+ *       ----------------------------------- 흐름
+ *    => 2차 프로젝트 : Spring(의존성 약한 프로그램)
+ *                   보안 / 권한 부여 / ORM(데이터베이스/Mybatis)
+ *          => JSP => MVC / Vuejs
+ *          => MyBatis
+ *    => 3차 프로젝트 : 신기술 적용 (Back / Front)
+ *                            --------------
+ *                             | 서버가 여러 개 MSA
+ *          => Spring Boot / Spring Cloud(MSA)
+ *          => MySQL , React(Redux) , JPA
+ *     ------------------------------------------
+ *       MyBatis / JPA
+ *                ------
+ *                
+ *       => 자바
+ *          프로그램 핵심 : 데이터 관리
+ *            변수 / 배열 / 클래스 / 파일 / 오라클 / XML / JSON
+ *            => 데이터베이스
+ *            => 머신러닝 / 딥러닝
+ *               => 데이터 수집 / 데이터 전처리 / 분석
+ *            ---------------------------------
+ *       => 프로젝트
+ *          => 벤치마킹
+ * */
+public class MainClass_2 {
+	public static void main(String[] args) {
+		
+	}
+}
